6d986939692af8035f3d688082f7808e17e7166f
Lionel Herrero Battista <lionherrero@gmail.com>
2025-06-03 11:19:50 -0300

    Added functionality for Flavor service. Added flavor display to Home component. Created model for Flavor. Added app's description in README.md

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FlavorService } from '../services/flavor.service';
-import { HttpClient } from '@angular/common/http';
+import { Flavor } from '../models/Flavor';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   private flavorService = inject(FlavorService);
-  private http = inject(HttpClient);
+  flavors!: Observable<Flavor[]>;
 
+  constructor() {
+    this.flavors = this.flavorService.getFlavors();
+  }
 }
--- src/app/services/flavor.service.ts
+++ src/app/services/flavor.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Flavor } from '../models/Flavor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlavorService {
+  private http = inject(HttpClient);
+  private baseUrl = "http://localhost:5299/api/flavor";
 
-  constructor() { }
+  getFlavors() {
+    return this.http.get<Flavor[]>(this.baseUrl);
+  }
+
 }
